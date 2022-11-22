de52db1d361efc5273e0c3b1982dcf5da7bdf5ff
Dawid P <dawid.paszkot98@gmail.com>
2022-11-22 10:08:34 +0100

    Fix for nav not generating projects list.

--- components/nav.php
+++ components/nav.php
@@ -1,6 +1,18 @@
 <?php
 function getProjectList(){
-    
+    $path = $_SERVER['DOCUMENT_ROOT'].$_SESSION['abs_path'].'/projects';
+
+    if(!is_dir($path)) {
+        echo '<script>alert("Directory \"projects\" not found! Creating one.");</script>';
+        mkdir($path, 0777, true);
+    }
+    $dir  = new DirectoryIterator($path);
+    foreach ($dir as $fileinfo) {
+        if ($fileinfo->isDir() && !$fileinfo->isDot()) {
+            $dirName = $fileinfo->getFilename();
+            echo "\n\t\t\t\t<li><a href=\"".$_SESSION['abs_path']."/projects/".$dirName."\">".$dirName."</a></li>\n\t\t\t";
+        }
+    }
 }
 ?>
 <div class="nav">
@@ -8,16 +20,7 @@ function getProjectList(){
         <li><a href=<?php echo $_SESSION['abs_path']."/index.php"; ?>>Home</a></li>
         <li><a href=<?php echo $_SESSION['abs_path']."/about.php"; ?>>O nas</a></li>
         <li><a href="#">Projekty</a>
-            <ul><?php
-               /* $path = $_SERVER['DOCUMENT_ROOT'].$_SESSION['abs_path'].'/projects';
-                $dir  = new DirectoryIterator($path);
-                foreach ($dir as $fileinfo) {
-                    if ($fileinfo->isDir() && !$fileinfo->isDot()) {
-                        $dirName = $fileinfo->getFilename();
-                        echo "\n\t\t\t\t<li><a href=\"".$_SESSION['abs_path']."/projects/".$dirName."\">".$dirName."</a></li>\n\t\t\t";
-                    }
-                } */
-            ?></ul>
+            <ul><?php getProjectList(); ?></ul>
         </li>
         <li><a href=<?php echo $_SESSION['abs_path']."/contact.php"; ?>>Kontakt</a></li>
         <li><?php
