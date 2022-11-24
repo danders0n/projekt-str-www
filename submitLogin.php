eb9f767e07c13550d5975381b89662395a9d93af
unknown <adrianna.saribekyan@gmail.com>
2022-11-24 19:34:16 +0100

    zabezpieczenia danych logowania, i poprawki nawiasow

--- submitLogin.php
+++ submitLogin.php
@@ -7,6 +7,14 @@ if(!isset($_SESSION['logged'])) {
     header('Location: about.php'); 
     exit();
 } */
+
+session_start();
+
+if((!isset($_POST['username'])) || (!isset($_POST['password']))){
+    header('Location: login.php');
+    exit();
+}
+
 require_once "connect.php"; //halko halko gdzie ten pliczor policja
 
 // Tworzenie polaczenia
@@ -17,28 +25,33 @@ if ($conn->connect_error!=0) {
     die("Connection failed: " . $conn->connect_error);
 }
 else{
-    echo "Connected successfully";
+    // echo "Connected successfully";
     $username=$_POST['username'];
     $password=$_POST['password'];
 
-    $sql = "SELECT username, password FROM users WHERE username = '$username' AND password = '$password'";
-    if ($answear=$conn->query($sql)){
+    // ochrona przed 'wstrzykiwaniem SQL'
+    $username=htmlentities($login, ENT_QUOTES, "UTF-8");
+    $username=htmlentities($password, ENT_QUOTES, "UTF-8");
+
+    $sql = ;
+    if ($answear=$conn->query(sprintf("SELECT username, password FROM users WHERE username = '%s AND password = '$%s'",
+    mysqli_real_escape_string($conn,$username), mysqli_real_escape_string($conn,$password))))   {
         $users_no = $answear->num_rows;
-    }
-    if($users_no==1){ // sprawdzanie, czy znajduje się (jedna) osoba o tych danych w DB
+        if($users_no==1){ // sprawdzanie, czy znajduje się (jedna) osoba o tych danych w DB
 
-        $_SESSION['logged'] = true;
+            $_SESSION['logged'] = true;
 
-        $line = $answear->fetch_assoc();
-        $username = $line['username'];
+            $line = $answear->fetch_assoc();
+            $username = $line['username'];
 
-        unset($_SESSION['invalid_password']);
-        $answear->free_result();
-        header('Location: about.php');
-    }
-    else{ // jeśli nie, ustawiona zmienna invalid_password i przeniesienie do strony logowania
-        $_SESSION['invalid_password'] = '<span style="color:red">Invalid username or password.</span>';
-        header('Location: login.php');
+            unset($_SESSION['invalid_password']);
+            $answear->free_result();
+            header('Location: about.php');
+        }
+        else { // jeśli nie, ustawiona zmienna invalid_password i przeniesienie do strony logowania
+            $_SESSION['invalid_password'] = '<span style="color:red">Invalid username or password.</span>';
+            header('Location: login.php');
+            }
     }
 
     $conn->close();
